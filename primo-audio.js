5e77705a4f3e13b11f18bc354fbf1ba1607585e5
Rob Ashton <robashton@codeofrob.com>
2013-01-13 02:19:39 +0000

    Dev deps + httpserver

--- primo-audio.js
+++ primo-audio.js
@@ -3,7 +3,9 @@ var Sound = function(path) {
   this.path = path
   this.loadedPath = ''
   this.rawdata = null
-  if(!this.audio) return
+  this.pool = false
+  if(!Sound.Enabled) return
+
   if(path.indexOf('.') > 0)
     this.loadAudioDirectly()
   else
@@ -22,27 +24,48 @@ Sound.prototype = {
     console.warn('Unable to load suitable audio for ', this.path)
   },
   loadmp3: function() {
-
-
+    if(!Sound.mp3) return false
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.onreadystatechange = function() {
+      if(xmlHttp.readyState === 4) {
+        console.log(xmlHttp.responseText)
+      }
+    }
+    xmlHttp.open( "GET", this.path + '.mp3.base64', true )
+    xmlHttp.send( null )
   },
   loadogg: function() {
+    if(!Sound.ogg) return false
 
 
   },
   loadaac: function() {
+    if(!Sound.aac) return false
 
 
   },
   loadwav: function() {
+    if(!Sound.wav) return false
 
-
+  },
+  play: function() {
+    if(!Sound.Enabled) return
+    var audio = this.getAudio()
+    audio.play()
+  },
+  getAudio: function() {
+    if(this.pool)
+      return this.findAvailablePooledAudio()
+    var audio = new Audio()
+    audio.src = this.loadedPath
+    return audio
   }
 }
 Sound.allowBase64 = true
 Sound.initSystem = function() {
   if(this.initialized) return
   var a = document.createElement('audio')
-  this.audio = !!a.canPlayType
+  this.Enabled = !!a.canPlayType
   this.mp3 = !!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))
   this.ogg = !!(a.canPlayType && a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''))
   this.wav = !!(a.canPlayType && a.canPlayType('audio/wav; codecs="1"').replace(/no/, ''))
